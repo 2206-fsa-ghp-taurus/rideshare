ca3f9418c91ec66f47e11f9fe5aabaa64ec640ae
Kandance Ferguson <kandance.ferguson@gmail.com>
2022-08-23 16:21:56 -0700

    messaging experimentation

--- src/App.js
+++ src/App.js
@@ -4,33 +4,34 @@ import UserMap from './components/UserMap';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import { AuthContext, useAuthInit } from './auth';
 import Login from './components/Login';
+import Messaging from './components/Messaging';
 
 const App = () => {
-  const {loading, authObj} = useAuthInit();
-  console.log('app is rendering with auth:', authObj)
+  const { loading, authObj } = useAuthInit();
+  console.log('app is rendering with auth:', authObj);
   if (loading) {
-    return <p> Loading Now </p>
+    return <p> Loading Now </p>;
   }
   return (
     <div>
-      <AuthContext.Provider value = {authObj}>
+      <AuthContext.Provider value={authObj}>
         <Switch>
-            {/* login route */}
-            <Route exact path = '/login'>
-              <Login/>
-            </Route>
-           
-           {/* home route, now rendering UerMap component */}
-            <Route exact path='/home'>
-              <UserMap />
-            </Route>
-            <Route exact path='/'>
-              <Redirect to='/home' />
-            </Route>
+          {/* login route */}
+          <Route exact path='/login'>
+            <Login />
+          </Route>
 
+          {/* home route, now rendering UserMap component */}
+          <Route exact path='/home'>
+            <UserMap />
+          </Route>
+          <Route exact path='/'>
+            <Redirect to='/home' />
+          </Route>
+          <Route path='/chat'></Route>
         </Switch>
       </AuthContext.Provider>
     </div>
-  )
-  };
+  );
+};
 export default App;
--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,23 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { getMessaging } from 'firebase/messaging';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA-ytfChvhv8nKAkh0CrPZPV5AqbKEKCF8",
-  authDomain: "capstone-a9136.firebaseapp.com",
-  projectId: "capstone-a9136",
-  storageBucket: "capstone-a9136.appspot.com",
-  messagingSenderId: "149550968258",
-  appId: "1:149550968258:web:eba3e31369697558613a5b",
-  measurementId: "G-DF1B3E3315"
+  apiKey: 'AIzaSyA-ytfChvhv8nKAkh0CrPZPV5AqbKEKCF8',
+  authDomain: 'capstone-a9136.firebaseapp.com',
+  projectId: 'capstone-a9136',
+  storageBucket: 'capstone-a9136.appspot.com',
+  messagingSenderId: '149550968258',
+  appId: '1:149550968258:web:eba3e31369697558613a5b',
+  measurementId: 'G-DF1B3E3315',
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export const db = getFirestore(app);
-export const auth = getAuth(app); 
+export const auth = getAuth(app);
+export const messaging = getMessaging(app);
+console.log('firebase set up');
