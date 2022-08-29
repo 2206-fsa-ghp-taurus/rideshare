bf5cbff4a1cf78a7776a34037aa70dfa0513656b
Kandance Ferguson <kandance.ferguson@gmail.com>
2022-08-29 12:24:03 -0400

    cleaned up api keys

--- src/components/Messaging.js
+++ src/components/Messaging.js
@@ -47,6 +47,7 @@ const Messaging = (props) => {
 
   setUserInfo();
 
+  //will need to be changed depending on what's able to be passed through props -- determining whether other user is a driver or rider
   const findRider = async () => {
     //going to pass ride id through props, once component is hooked up
     //might pass all ride info. Can clean up thiscode and pull directly from props
@@ -67,6 +68,7 @@ const Messaging = (props) => {
 
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
+      {/* pass throguh props, either rider or driver's id depending on current status; delete accounts upon closing chat in the next view*/}
       <CometChatMessages chatWithUser={findRider()} />
     </div>
   );
--- src/components/Routing.js
+++ src/components/Routing.js
@@ -36,9 +36,7 @@ const Routing = (props) => {
       L.latLng(pickUpLat, pickUpLng),
       L.latLng(dropOffLat, dropOffLng),
     ],
-    router: L.Routing.mapbox(
-      'sk.eyJ1Ijoia2xldmluZTg4IiwiYSI6ImNsNzUxeDVoeTFuazUzcG1xb3ZuOGd3aXcifQ.gTCOe2GB8DcStiCKcoowJw'
-    ),
+    router: L.Routing.mapbox(`${process.env.REACT_APP_MAP_BOX_API_KEY}`),
     createMarker: function (i, start, n) {
       //for (i = 0; waypoint.length; i++){
       return L.marker(start.latLng, { icon: greenIcon })
