e1ebf3100b51ae2f52640defbfeb251620d2995c
Sher <siaufui327@hotmail.com>
2022-08-30 19:02:28 +0100

    allchanges

--- src/components/Messaging.js
+++ src/components/Messaging.js
@@ -21,9 +21,6 @@ CometChat.init(CONSTANTS.APP_ID, appSetting).then(
   }
 );
 
-
-
-
 const Messaging = (props) => {
   const { userId } = useAuth();
   console.log(props);
@@ -32,38 +29,37 @@ const Messaging = (props) => {
   const { riderId } = props;
   const { isDriver } = props;
 
-//   const userInfo = async () => {
-//     const userName = await getDoc(doc(db, 'Users', userId));
-//     const firstName = userName.data().firstName;
-//     return firstName;
-//   };
+  const userInfo = async () => {
+    const userName = await getDoc(doc(db, 'Users', userId));
+    const firstName = userName.data().firstName;
+    return firstName;
+  };
 
-//   const setUserInfo = async () => {
-//     let user = new CometChat.User(userId);
-//     let firstName = await userInfo();
-//     user.setName(firstName);
+  const setUserInfo = async () => {
+    let user = new CometChat.User(userId);
+    let firstName = await userInfo();
+    user.setName(firstName);
 
-//     CometChat.createUser(user, CONSTANTS.AUTH_KEY).then(
-//       (user) => {
-//         console.log('user created', user);
-//       },
-//       (error) => {
-//         console.log('error', error);
-//       }
-//     );
-//   };
+    CometChat.createUser(user, CONSTANTS.AUTH_KEY).then(
+      (user) => {
+        console.log('user created', user);
+      },
+      (error) => {
+        console.log('error', error);
+      }
+    );
+  };
 
-//   setUserInfo();
+  setUserInfo();
 
-//   //will need to be changed depending on what's able to be passed through props -- determining whether other user is a driver or rider
-//   const findRider = async () => {
-//     //going to pass ride id through props, once component is hooked up
-//     //might pass all ride info. Can clean up thiscode and pull directly from props
-//     let rideId = 'xo4NYD3evPjSfwpClGXq'; //change to value on props
-//     let rideInfo = await getDoc(doc(db, 'Rides', rideId));
-//     const riderId = rideInfo.data().riderId;
-//     const status = rideInfo.data().status;
-//     const driverId = rideInfo.data().driverId;
+  //will need to be changed depending on what's able to be passed through props -- determining whether other user is a driver or rider
+  // const findUser = async () => {
+  //   //going to pass ride id through props, once component is hooked up
+  //   //might pass all ride info. Can clean up thiscode and pull directly from props
+  //   let rideInfo = await getDoc(doc(db, 'Rides', rideId));
+  //   const riderId = rideInfo.data().riderId;
+  //   const status = rideInfo.data().status;
+  //   const driverId = rideInfo.data().driverId;
 
   //   if (driverId === userId && status > 0) {
   //     return await riderId;
@@ -72,14 +68,14 @@ const Messaging = (props) => {
   //   }
   // };
 
-//   //query rides db for status (), pull current ride
+  //   //query rides db for status (), pull current ride
 
-//   return (
-//     <div style={{ width: '100vw', height: '100vh' }}>
-//       {/* pass throguh props, either rider or driver's id depending on current status; delete accounts upon closing chat in the next view*/}
-//       <CometChatMessages chatWithUser={findRider()} />
-//     </div>
-//   );
-// };
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      {/* pass throguh props, either rider or driver's id depending on current status; delete accounts upon closing chat in the next view*/}
+      <CometChatMessages chatWithUser={isDriver ? riderId : driverId} />
+    </div>
+  );
+};
 
-// export default Messaging;
+export default Messaging;
